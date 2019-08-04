62452d18c8c222c971e3aa104d786bc9abd06a6d
Leo <leo881003@gmail.com>
2019-08-04 12:12:58 +0800

    Completed basic parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,182 @@
-use pest::Parser;
+use pest::{error::*, Parser, Position};
+
+lazy_static! {
+    static ref RFC1421_CFG: base64::Config =
+        base64::Config::new(base64::CharacterSet::Standard, true).decode_allow_trailing_bits(true);
+}
+
+fn rfc1421_base64_decode<T: ?Sized + AsRef<[u8]>>(
+    input: &T,
+) -> Result<Vec<u8>, base64::DecodeError> {
+    base64::decode_config(input, *RFC1421_CFG)
+}
 
 #[derive(Parser)]
 #[grammar = "pem.pest"]
 struct PemParser;
+
+fn pem_parser(input: &str) -> Result<(), Error<Rule>> {
+    let mut pem_pairs = PemParser::parse(Rule::pem, input)?;
+    if let Some(pem_tokens) = pem_pairs.next() {
+        for portions in pem_tokens.into_inner() {
+            match portions.as_rule() {
+                Rule::pre_eb => {
+                    let mut eb_pairs = portions.into_inner();
+                    let label = eb_pairs.next().unwrap().as_str();
+                }
+                Rule::post_eb => (),
+                Rule::content => {
+                    let mut raw_content = String::new();
+                    for content_line in portions.as_str().lines() {
+                        raw_content.push_str(content_line.trim());
+                    }
+                    let data = rfc1421_base64_decode(&raw_content).map_err(|err| {
+                        Error::new_from_span(custom_error(&err.to_string()), portions.as_span())
+                    })?;
+                    dbg!(&data);
+                }
+                Rule::headers => {
+                    let headers_pairs = portions.into_inner();
+                    let mut headers = Vec::with_capacity(headers_pairs.size_hint().0);
+                    for header_pair in headers_pairs {
+                        let mut header_inner = header_pair.into_inner();
+                        let name = header_inner.next().unwrap().as_str();
+                        let mut body = String::new();
+                        for line in header_inner.next().unwrap().as_str().lines() {
+                            body.push_str(line.trim());
+                        }
+                        headers.push((name, body));
+                    }
+                }
+                _ => unreachable!(),
+            }
+        }
+        Ok(())
+    } else {
+        Err(Error::new_from_pos(
+            custom_error("Missing PEM block"),
+            Position::from_start(input),
+        ))
+    }
+}
+
+fn custom_error(message: &str) -> ErrorVariant<Rule> {
+    ErrorVariant::CustomError {
+        message: message.to_owned(),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const RFC1421_FIGURE2: &str = "-----BEGIN PRIVACY-ENHANCED MESSAGE-----
+Proc-Type: 4,ENCRYPTED
+Content-Domain: RFC822
+DEK-Info: DES-CBC,F8143EDE5960C597
+Originator-ID-Symmetric: linn@zendia.enet.dec.com,,
+Recipient-ID-Symmetric: linn@zendia.enet.dec.com,ptf-kmc,3
+Key-Info: DES-ECB,RSA-MD2,9FD3AAD2F2691B9A,
+          B70665BB9BF7CBCDA60195DB94F727D3
+Recipient-ID-Symmetric: pem-dev@tis.com,ptf-kmc,4
+Key-Info: DES-ECB,RSA-MD2,161A3F75DC82EF26,
+          E2EF532C65CBCFF79F83A2658132DB47
+
+LLrHB0eJzyhP+/fSStdW8okeEnv47jxe7SJ/iN72ohNcUk2jHEUSoH1nvNSIWL9M
+8tEjmF/zxB+bATMtPjCUWbz8Lr9wloXIkjHUlBLpvXR0UrUzYbkNpk0agV2IzUpk
+J6UiRRGcDSvzrsoK+oNvqu6z7Xs5Xfz5rDqUcMlK1Z6720dcBWGGsDLpTpSCnpot
+dXd/H5LMDWnonNvPCwQUHt==
+-----END PRIVACY-ENHANCED MESSAGE-----
+";
+
+    const RFC1421_FIGURE3: &str = "-----BEGIN PRIVACY-ENHANCED MESSAGE-----
+Proc-Type: 4,ENCRYPTED
+Content-Domain: RFC822
+DEK-Info: DES-CBC,BFF968AA74691AC1
+Originator-Certificate:
+ MIIBlTCCAScCAWUwDQYJKoZIhvcNAQECBQAwUTELMAkGA1UEBhMCVVMxIDAeBgNV
+ BAoTF1JTQSBEYXRhIFNlY3VyaXR5LCBJbmMuMQ8wDQYDVQQLEwZCZXRhIDExDzAN
+ BgNVBAsTBk5PVEFSWTAeFw05MTA5MDQxODM4MTdaFw05MzA5MDMxODM4MTZaMEUx
+ CzAJBgNVBAYTAlVTMSAwHgYDVQQKExdSU0EgRGF0YSBTZWN1cml0eSwgSW5jLjEU
+ MBIGA1UEAxMLVGVzdCBVc2VyIDEwWTAKBgRVCAEBAgICAANLADBIAkEAwHZHl7i+
+ yJcqDtjJCowzTdBJrdAiLAnSC+CnnjOJELyuQiBgkGrgIh3j8/x0fM+YrsyF1u3F
+ LZPVtzlndhYFJQIDAQABMA0GCSqGSIb3DQEBAgUAA1kACKr0PqphJYw1j+YPtcIq
+ iWlFPuN5jJ79Khfg7ASFxskYkEMjRNZV/HZDZQEhtVaU7Jxfzs2wfX5byMp2X3U/
+ 5XUXGx7qusDgHQGs7Jk9W8CW1fuSWUgN4w==
+Key-Info: RSA,
+ I3rRIGXUGWAF8js5wCzRTkdhO34PTHdRZY9Tuvm03M+NM7fx6qc5udixps2Lng0+
+ wGrtiUm/ovtKdinz6ZQ/aQ==
+Issuer-Certificate:
+ MIIB3DCCAUgCAQowDQYJKoZIhvcNAQECBQAwTzELMAkGA1UEBhMCVVMxIDAeBgNV
+ BAoTF1JTQSBEYXRhIFNlY3VyaXR5LCBJbmMuMQ8wDQYDVQQLEwZCZXRhIDExDTAL
+ BgNVBAsTBFRMQ0EwHhcNOTEwOTAxMDgwMDAwWhcNOTIwOTAxMDc1OTU5WjBRMQsw
+ CQYDVQQGEwJVUzEgMB4GA1UEChMXUlNBIERhdGEgU2VjdXJpdHksIEluYy4xDzAN
+ BgNVBAsTBkJldGEgMTEPMA0GA1UECxMGTk9UQVJZMHAwCgYEVQgBAQICArwDYgAw
+ XwJYCsnp6lQCxYykNlODwutF/jMJ3kL+3PjYyHOwk+/9rLg6X65B/LD4bJHtO5XW
+ cqAz/7R7XhjYCm0PcqbdzoACZtIlETrKrcJiDYoP+DkZ8k1gCk7hQHpbIwIDAQAB
+ MA0GCSqGSIb3DQEBAgUAA38AAICPv4f9Gx/tY4+p+4DB7MV+tKZnvBoy8zgoMGOx
+ dD2jMZ/3HsyWKWgSF0eH/AJB3qr9zosG47pyMnTf3aSy2nBO7CMxpUWRBcXUpE+x
+ EREZd9++32ofGBIXaialnOgVUn0OzSYgugiQ077nJLDUj0hQehCizEs5wUJ35a5h
+MIC-Info: RSA-MD5,RSA,
+ UdFJR8u/TIGhfH65ieewe2lOW4tooa3vZCvVNGBZirf/7nrgzWDABz8w9NsXSexv
+ AjRFbHoNPzBuxwmOAFeA0HJszL4yBvhG
+Recipient-ID-Asymmetric:
+ MFExCzAJBgNVBAYTAlVTMSAwHgYDVQQKExdSU0EgRGF0YSBTZWN1cml0eSwgSW5j
+ LjEPMA0GA1UECxMGQmV0YSAxMQ8wDQYDVQQLEwZOT1RBUlk=,
+ 66
+Key-Info: RSA,
+ O6BS1ww9CTyHPtS3bMLD+L0hejdvX6Qv1HK2ds2sQPEaXhX8EhvVphHYTjwekdWv
+ 7x0Z3Jx2vTAhOYHMcqqCjA==
+
+qeWlj/YJ2Uf5ng9yznPbtD0mYloSwIuV9FRYx+gzY+8iXd/NQrXHfi6/MhPfPF3d
+jIqCJAxvld2xgqQimUzoS1a4r7kQQ5c/Iua4LqKeq3ciFzEv/MbZhA==
+-----END PRIVACY-ENHANCED MESSAGE-----
+";
+
+    const RFC1421_FIGURE4: &str = "-----BEGIN PRIVACY-ENHANCED MESSAGE-----
+Proc-Type: 4,MIC-ONLY
+Content-Domain: RFC822
+Originator-Certificate:
+ MIIBlTCCAScCAWUwDQYJKoZIhvcNAQECBQAwUTELMAkGA1UEBhMCVVMxIDAeBgNV
+ BAoTF1JTQSBEYXRhIFNlY3VyaXR5LCBJbmMuMQ8wDQYDVQQLEwZCZXRhIDExDzAN
+ BgNVBAsTBk5PVEFSWTAeFw05MTA5MDQxODM4MTdaFw05MzA5MDMxODM4MTZaMEUx
+ CzAJBgNVBAYTAlVTMSAwHgYDVQQKExdSU0EgRGF0YSBTZWN1cml0eSwgSW5jLjEU
+ MBIGA1UEAxMLVGVzdCBVc2VyIDEwWTAKBgRVCAEBAgICAANLADBIAkEAwHZHl7i+
+ yJcqDtjJCowzTdBJrdAiLAnSC+CnnjOJELyuQiBgkGrgIh3j8/x0fM+YrsyF1u3F
+ LZPVtzlndhYFJQIDAQABMA0GCSqGSIb3DQEBAgUAA1kACKr0PqphJYw1j+YPtcIq
+ iWlFPuN5jJ79Khfg7ASFxskYkEMjRNZV/HZDZQEhtVaU7Jxfzs2wfX5byMp2X3U/
+ 5XUXGx7qusDgHQGs7Jk9W8CW1fuSWUgN4w==
+Issuer-Certificate:
+ MIIB3DCCAUgCAQowDQYJKoZIhvcNAQECBQAwTzELMAkGA1UEBhMCVVMxIDAeBgNV
+ BAoTF1JTQSBEYXRhIFNlY3VyaXR5LCBJbmMuMQ8wDQYDVQQLEwZCZXRhIDExDTAL
+ BgNVBAsTBFRMQ0EwHhcNOTEwOTAxMDgwMDAwWhcNOTIwOTAxMDc1OTU5WjBRMQsw
+ CQYDVQQGEwJVUzEgMB4GA1UEChMXUlNBIERhdGEgU2VjdXJpdHksIEluYy4xDzAN
+ BgNVBAsTBkJldGEgMTEPMA0GA1UECxMGTk9UQVJZMHAwCgYEVQgBAQICArwDYgAw
+ XwJYCsnp6lQCxYykNlODwutF/jMJ3kL+3PjYyHOwk+/9rLg6X65B/LD4bJHtO5XW
+ cqAz/7R7XhjYCm0PcqbdzoACZtIlETrKrcJiDYoP+DkZ8k1gCk7hQHpbIwIDAQAB
+ MA0GCSqGSIb3DQEBAgUAA38AAICPv4f9Gx/tY4+p+4DB7MV+tKZnvBoy8zgoMGOx
+ dD2jMZ/3HsyWKWgSF0eH/AJB3qr9zosG47pyMnTf3aSy2nBO7CMxpUWRBcXUpE+x
+ EREZd9++32ofGBIXaialnOgVUn0OzSYgugiQ077nJLDUj0hQehCizEs5wUJ35a5h
+MIC-Info: RSA-MD5,RSA,
+ jV2OfH+nnXHU8bnL8kPAad/mSQlTDZlbVuxvZAOVRZ5q5+Ejl5bQvqNeqOUNQjr6
+ EtE7K2QDeVMCyXsdJlA8fA==
+
+LSBBIG1lc3NhZ2UgZm9yIHVzZSBpbiB0ZXN0aW5nLg0KLSBGb2xsb3dpbmcgaXMg
+YSBibGFuayBsaW5lOg0KDQpUaGlzIGlzIHRoZSBlbmQuDQo=
+-----END PRIVACY-ENHANCED MESSAGE-----";
+
+    #[test]
+    fn pem_parse_figure2() {
+        pem_parser(RFC1421_FIGURE2).unwrap();
+    }
+
+    #[test]
+    fn pem_parse_figure3() {
+        pem_parser(RFC1421_FIGURE3).unwrap();
+    }
+
+    #[test]
+    fn pem_parse_figure4() {
+        pem_parser(RFC1421_FIGURE4).unwrap();
+    }
+}
